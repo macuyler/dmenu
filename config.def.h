99db9733bf0c0d9d8af5fd3bdf84d0a1d5d83023
Macuyler Dunn <macuyler@protonmail.com>
2022-06-18 10:14:15 -0500

    Update configuration
    - Add custom color scheme.
    - Add custom prompt.
    - Add noto mono font.

--- config.def.h
+++ config.def.h
@@ -4,14 +4,14 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Noto Mono:size=10"
 };
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *prompt      = "  % ";      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeNorm] = { "#dfdfdf", "#1d1d1d" },
+	[SchemeSel] = { "#f9f5d7", "#076678" },
+	[SchemeOut] = { "#000000", "#a4a4a4" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
